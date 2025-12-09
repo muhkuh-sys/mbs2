e70aa7ab7973a7da5852a19e496dd9b89538abc3
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2025-12-09 15:29:09 +0100

    Manage environments with an ID.

--- local/mbs2/init.lua
+++ local/mbs2/init.lua
@@ -75,7 +75,12 @@ end
 
 
 
-function cMbs:createEnv(atRequiredTools, tCfg)
+function cMbs:createEnv(strID, atRequiredTools, tCfg)
+  -- Does an environment whith this ID already exist?
+  if self.atEnv[strID]~=nil then
+    error('An environment with the ID "' .. strID .. '" already exists.')
+  end
+
   -- Create a new environment.
   local tEnv = NewSettings()
   local strError
@@ -88,6 +93,9 @@ function cMbs:createEnv(atRequiredTools, tCfg)
 
   -- Unlock the settings table to add the "mbs" object and all tools and builders.
   require 'mbs2/table_unlock'.TableUnlock(tEnv)
+
+  -- Set the ID.
+  tEnv.mbs_id = strID
 
   -- Add the "mbs" table as a key-value store which can be used in builders.
   -- Populate it with a st of known values from the configuration, like the asic typ.
@@ -149,6 +157,83 @@ function cMbs:createEnv(atRequiredTools, tCfg)
     error(strError)
   else
     TableLock(tEnv)
+
+    -- Register the new environment.
+    self.atEnv[strID] = tEnv
+  end
+end
+
+
+
+function cMbs:hasEnv(strID)
+  local tEnv = self.atEnv[strID]
+  return (tEnv~=nil)
+end
+
+
+
+function cMbs:cloneEnv(strID, tCfg)
+  local tEnvMaster = self.atEnv[strID]
+  if tEnvMaster==nil then
+    error('An environment with the ID "' .. strID .. '" was not found.')
+  end
+
+  local tEnv = tEnvMaster:Copy()
+
+  -- Set the label for all output lines, if there is something in the configuration.
+  local strLabel = tCfg.label
+  if type(strLabel)=='string' then
+    tEnv.labelprefix = '[' .. strLabel .. '] '
+  end
+
+  return tEnv
+end
+
+
+
+function cMbs:mergeEnv(tID, atKeyValuePairs)
+  local strID
+  if type(tID)=='string' then
+    strID = tID
+  elseif type(tID)=='table' and type(tID.mbs_id)=='string' then
+    strID = tID.mbs_id
+  else
+    error('First argument must be a string or an environment object.')
+  end
+
+  local tEnv = self.atEnv[strID]
+  if tEnv==nil then
+    error('An environment with the ID "' .. strID .. '" was not found.')
+  end
+
+  local tMbs = tEnv.mbs
+  for strKey, tValue in pairs(atKeyValuePairs) do
+    tMbs[strKey] = tValue
+  end
+
+  return tEnv
+end
+
+
+
+function cMbs:cloneAnyEnv(tCfg)
+  -- To get a reproducable behaviour, sort the available keys and take the first one.
+  local astrKeys = {}
+  for strKey in pairs(self.atEnv) do
+    table.insert(astrKeys, strKey)
+  end
+  table.sort(astrKeys)
+  if #astrKeys==0 then
+    error('No environments found.')
+  end
+  local tEnvMaster = self.atEnv[astrKeys[1]]
+
+  local tEnv = tEnvMaster:Copy()
+
+  -- Set the label for all output lines, if there is something in the configuration.
+  local strLabel = tCfg.label
+  if type(strLabel)=='string' then
+    tEnv.labelprefix = '[' .. strLabel .. '] '
   end
 
   return tEnv
