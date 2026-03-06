37ee53129b00dd95488f3acd7cd8094de8f338a3
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2026-03-06 14:11:15 +0100

    Generate a compile_commands file in a very unelegant way.

--- local/mbs2/builder/gcc.lua
+++ local/mbs2/builder/gcc.lua
@@ -49,6 +49,104 @@ end
 
 
 
+function DriverGCC_Get(exe, cache_name, flags_name)
+  return function(label, output, input, settings)
+    local cache = settings.cc[cache_name]
+    if settings.invoke_count ~= cache.nr then
+      cache.nr = settings.invoke_count
+      local cc = settings.cc
+      local d = TableToString(cc.defines, "-D", " ")
+      local i = TableToString(cc.includes, '-I "', '" ') ..
+                TableToString(cc.systemincludes, '-isystem "', '" ') ..
+                TableToString(cc.frameworks, '-framework ', ' ')
+      local f = cc.flags:ToString()
+      f = f .. cc[flags_name]:ToString()
+      if settings.debug > 0 then
+        f = f .. "-g "
+      end
+      if settings.optimize > 0 then
+        f = f .. "-O2 "
+      end
+
+      cache.str = cc[exe] .. " " .. f .. "-c " .. d .. i .. " -o "
+    end
+
+    local strCompileCommandsFile
+    local tMbs = settings.mbs
+    if type(tMbs)=='table' then
+      strCompileCommandsFile = tMbs.COMPILE_COMMANDS_FILE
+      local atCompileCommands = tMbs.COMPILE_COMMANDS
+      local strCompileCommandsCwd = tMbs.COMPILE_COMMANDS_CWD
+
+      if(
+        type(strCompileCommandsFile)=='string' and
+        type(atCompileCommands)=='table' and
+        type(strCompileCommandsCwd)=='string'
+      ) then
+        local cc = settings.cc
+        local astrArgs = {
+          cc[exe],
+        }
+        for _, strFlag in ipairs(cc.flags) do
+          table.insert(astrArgs, strFlag)
+        end
+        for _, strFlag in ipairs(cc[flags_name]) do
+          table.insert(astrArgs, strFlag)
+        end
+        if settings.debug > 0 then
+          table.insert(astrArgs, '-g')
+        end
+        if settings.optimize > 0 then
+          table.insert(astrArgs, '-O2')
+        end
+        table.insert(astrArgs, '-c')
+        for _, strDefine in ipairs(cc.defines) do
+          table.insert(astrArgs, '-D' .. strDefine)
+        end
+        for _, strInclude in ipairs(cc.includes) do
+          table.insert(astrArgs, '-I' .. strInclude)
+        end
+        for _, strInclude in ipairs(cc.systemincludes) do
+          table.insert(astrArgs, '-isystem')
+          table.insert(astrArgs, '"' .. strInclude .. '"')
+        end
+        for _, strFramework in ipairs(cc.frameworks) do
+          table.insert(astrArgs, '-framework')
+          table.insert(astrArgs, strFramework)
+        end
+        table.insert(astrArgs, '-o')
+        table.insert(astrArgs, output)
+        table.insert(astrArgs, input)
+
+        table.insert(
+          atCompileCommands,
+          {
+            directory = strCompileCommandsCwd,
+            arguments = astrArgs,
+            file = input
+          }
+        )
+
+        -- FIXME: This writes the compile commands file again and again for each file to be compiled.
+        --        A better solution is a special target for the compile_commands.json file which depends
+        --        on all source files.
+        local tFile = io.open(strCompileCommandsFile, 'w')
+        if tFile~=nil then
+          tFile:write(require 'rapidjson'.encode(atCompileCommands))
+          tFile:close()
+        end
+      end
+    end
+
+    AddJob(output, label, cache.str .. output .. " " .. input)
+--    if type(strCompileCommandsFile)=='string' then
+--      AddDependency(output, strCompileCommandsFile)
+--    end
+  end
+end
+
+
+
 function tBuilder.DriverGCC_Link(label, output, inputs, settings)
   -- Remove the first file with an "ld" extension from the inputs and use it as the linker description file.
   local strLdFileArg
@@ -85,9 +183,21 @@ end
 
 
 function tBuilder:applyToEnv(tEnv, tCfg)
+  tEnv.cc.DriverC = DriverGCC_Get('exe_c', '_c_cache', 'flags_c')
+  tEnv.cc.DriverCXX = DriverGCC_Get('exe_cxx', '_cxx_cache', 'flags_cxx')
   tEnv.cc.Output = self.translateFilename
 
   tEnv.link.Driver = self.DriverGCC_Link
+
+  function tEnv:CompileCommands(strTargetPath)
+    local tMbs = self.mbs
+    tMbs.COMPILE_COMMANDS_FILE = strTargetPath
+    tMbs.COMPILE_COMMANDS_CWD = require 'pl.path'.currentdir()
+    tMbs.COMPILE_COMMANDS = {}
+
+    local strLabel = tEnv.labelprefix .. 'CompileCommands ' .. strTargetPath
+--    AddJob(strTargetPath, strLabel, cache.str .. output .. " " .. input)
+  end
 
   function tEnv:SetBuildPath(strOutputFolder, strInputPrefix)
     self.mbs.TRANSLATE_FILENAME_OUTPUT_FOLDER = strOutputFolder
@@ -120,6 +230,9 @@ function tBuilder:applyToEnv(tEnv, tCfg)
   local tMbs = tEnv.mbs
   tMbs.TRANSLATE_FILENAME_OUTPUT_FOLDER = ''
   tMbs.TRANSLATE_FILENAME_INPUT_PREFIX = ''
+  tMbs.COMPILE_COMMANDS_FILE = nil
+  tMbs.COMPILE_COMMANDS_CWD = nil
+  tMbs.COMPILE_COMMANDS = nil
 
   return true
 end
